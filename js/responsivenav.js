540cab3b24e4e02bb21321f3fc21ab2002cb3383
vitorregisr <vitorregisrr@gmail.com>
2019-10-26 16:23:06 -0300

    Add home and main-header responsive and styled

--- js/responsivenav.js
+++ js/responsivenav.js
@@ -4,6 +4,13 @@
     $('[data-toggle="mobile-nav"]').click(function () {
         $('#mobile-nav').toggleClass('active');
         $('#mobile-nav').attr('aria-hidden', $('#mobile-nav').attr('aria-hidden') === "true" ? false : true);
+        
+        $('#mobile-nav .animate').addClass('animated');
+        $('#mobile-nav .animate').css('display', 'none');
+        
+        setTimeout(() => {
+            $('#mobile-nav .animate').css('display', 'block');
+        }, 100)
     });
     
     $('#mobile-nav li a').click(function () {
